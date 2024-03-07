c76cbd41d67b4a7328d947ee1fdd0fbddbe0da88
sabiraniiazbaeva <sabiraniyazbaeva@gamil.com>
2024-03-07 03:59:29 +0000

    3

--- do-droplet-creation/main.tf
+++ do-droplet-creation/main.tf
@@ -4,7 +4,6 @@ resource "digitalocean_droplet" "master" {
   region = var.region
   size   = var.master_size
   ssh_keys = [ var.ssh_key_fingerprint ]
-  #   var.ssh_fingerprint_2,
 
 } 
 
@@ -15,7 +14,6 @@ resource "digitalocean_droplet" "worker1" {
   region = var.region
   size   = var.worker1_size
   ssh_keys = [ var.ssh_key_fingerprint ]
-  # ssh_keys = [digitalocean_ssh_key.default.fingerprint]
 } 
 
 resource "digitalocean_droplet" "worker2" {
@@ -24,8 +22,5 @@ resource "digitalocean_droplet" "worker2" {
   region = var.region
   size   = var.worker2_size
   ssh_keys = [ var.ssh_key_fingerprint ]
-#   ssh_keys = [
-#     var.ssh_fingerprint_1,
-#     var.ssh_fingerprint_2,
-# ]
+  
 } 
